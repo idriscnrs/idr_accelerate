8a68ff9cf2d8b6bba097023498469bcf5facba8d
hatimbr <hatim.bourfoune@idris.fr>
2023-05-02 15:49:11 +0200

    import in gitlab

---
+++ example_finetuning_accelerate.py
@@ -0,0 +1,124 @@
+import os
+from time import time
+import torch
+
+from tqdm.auto import tqdm
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from accelerate import Accelerator
+
+from datasets import load_from_disk
+from torchmetrics.classification import BinaryAccuracy
+
+accelerator = Accelerator()
+DEVICE = accelerator.device
+
+torch.manual_seed(53)
+torch.cuda.manual_seed(53)
+
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default='/gpfsdswork/dataset/HuggingFace/imdb/plain_text')
+    parser.add_argument('--model_dir', type=str, default='/gpfsdswork/dataset/HuggingFace_Models/')
+    parser.add_argument('--model_name', type=str, default='distilbert-base-uncased')
+    parser.add_argument('--epochs', type=int, default=1)
+    parser.add_argument('--lr', type=float, default=1e-04)
+    parser.add_argument('--batch_size', type=int, default=32)
+    args = parser.parse_args()
+    return args
+
+
+def train_loop(model, tokenizer, train_dataloader, criterion, optimizer):
+    model.train()
+    loop = tqdm(train_dataloader)
+
+    for data in loop:
+        inputs = tokenizer.batch_encode_plus(
+            data['text'],
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=512
+        ).to(DEVICE)
+        labels = data['label'].to(DEVICE)
+
+        out = model(**inputs)
+        loss = criterion(out.logits, labels)
+
+        accelerator.backward(loss)
+        optimizer.step()
+
+        loop.set_postfix(loss=loss.item())
+
+    return model
+
+
+def eval_loop(model, tokenizer, test_dataloader):
+    metric = BinaryAccuracy().to(DEVICE)
+    model.eval()
+    loop = tqdm(test_dataloader)
+    with torch.no_grad():
+        for data in loop:
+            inputs = tokenizer.batch_encode_plus(
+                data['text'],
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+                max_length=512
+            ).to(DEVICE)
+            labels = data['label'].to(DEVICE)
+
+            out = model(**inputs)
+            score = metric(out.logits.argmax(dim=1), labels)
+
+            loop.set_postfix(weighted_average_score=metric.compute().item(), score=score.item())
+
+    return metric.compute()
+
+
+def main(args):
+    # Initialize Datasets
+    dataset = load_from_disk(args.data_path)
+
+    train_dataloader = torch.utils.data.DataLoader(
+        dataset['train'],
+        batch_size=args.batch_size,
+        num_workers=4,
+        pin_memory=True,
+        prefetch_factor=2,
+        shuffle=True
+    )
+
+    test_dataloader = torch.utils.data.DataLoader(
+        dataset['test'],
+        batch_size=args.batch_size,
+        num_workers=4,
+        pin_memory=True,
+        prefetch_factor=2,
+    )
+
+    # Initialize Model and Tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(os.path.join(args.model_dir, args.model_name))
+    model = AutoModelForSequenceClassification.from_pretrained(os.path.join(args.model_dir, args.model_name)).to(DEVICE)
+
+    # Initialize Optimizer and Criterion
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+
+    model, optimizer, train_dataloader, test_dataloader = accelerator.prepare(
+        model, optimizer, train_dataloader, test_dataloader
+    )
+
+    for epoch in range(args.epochs):
+        print(f"######################## Epoch {epoch+1}/{args.epochs} ########################")
+        start_epoch = time()
+        model = train_loop(model, tokenizer, train_dataloader, criterion, optimizer)
+        accuracy = eval_loop(model, tokenizer, test_dataloader)
+        print(f"\nEpoch: {epoch+1}/{args.epochs} | Duration: {(time() - start_epoch):.3f}s | Accuracy: {accuracy}")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
---
+++ idr_accelerate.py
@@ -0,0 +1,37 @@
+import os
+import yaml
+
+master_addr = os.environ['SLURMD_NODENAME']
+master_port = 12346
+num_machines = int(os.environ['SLURM_NNODES'])
+nb_gpus = len(os.environ['SLURM_JOB_GPUS'].split(','))
+num_processes = num_machines * nb_gpus
+
+
+for machine_rank in range(num_machines):
+    config_accelerate = {
+        'compute_environment': 'LOCAL_MACHINE',
+        'deepspeed_config': {},
+        'distributed_type': 'MULTI_GPU',
+        'downcast_bf16': 'no',
+        'dynamo_backend': 'NO',
+        'fsdp_config': {},
+        'gpu_ids': 'all',
+        'machine_rank': machine_rank,
+        'main_process_ip': master_addr,
+        'main_process_port': master_port,
+        'main_training_function': 'main',
+        'megatron_lm_config': {},
+        'mixed_precision': 'no',
+        'num_machines': num_machines,
+        'num_processes': num_processes,
+        'rdzv_backend': 'static',
+        'same_network': True,
+        'tpu_env': [],
+        'tpu_use_cluster': False,
+        'tpu_use_sudo': False,
+        'use_cpu': False,
+    }
+
+    with open(f"./config_accelerate_rank{machine_rank}.yaml", "w") as file:
+        yaml.dump(config_accelerate, file)
