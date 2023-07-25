b9bdc99c5bdac70c4d3b487731d3b0f065a255d6
Nathan CASSEREAU <nathan.cassereau@idris.fr>
2023-07-25 14:09:18 +0000

    Idr_accelerate as a launcher

---
+++ setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="idr_accelerate",
+    version="2.0.0",
+    author="IDRIS",
+    author_email="assist@idris.fr",
+    url="https://www.idris.fr",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'idr_accelerate = idr_accelerate.launcher:run'
+        ],
+    },
+    install_requires=[
+        "accelerate",
+        "idr_torch>=2.0.0",
+        "packaging",
+        "torch",
+    ],
+    license="MIT",
+)
