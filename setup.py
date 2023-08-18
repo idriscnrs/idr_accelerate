850136f264cd4d7095303c17869b923430c6554a
Nathan Cassereau <nathan.cassereau@idris.fr>
2023-08-18 11:23:59 +0200

    Add single source version and __version__ attribute

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 from setuptools import find_packages, setup
 
+PACKAGE_NAME = "idr_accelerate"
+VERSIONFILE = "VERSION"
+AUTHOR = "IDRIS"
+AUTHOR_EMAIL = "assist@idris.fr"
+URL = "https://www.idris.fr"
+
+with open(os.path.join(PACKAGE_NAME, VERSIONFILE), "r") as file:
+    VERSION = file.read().strip()
+
 setup(
-    name="idr_accelerate",
-    version="2.0.0",
-    author="IDRIS",
-    author_email="assist@idris.fr",
-    url="https://www.idris.fr",
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
     packages=find_packages(),
     entry_points={
         "console_scripts": ["idr_accelerate = idr_accelerate.launcher:run"],
@@ -19,5 +30,6 @@ setup(
         "packaging",
         "torch",
     ],
+    include_package_data=True,
     license="MIT",
 )
