b247ffd554045c9f0f318b280ee0edfe6c5a6554
Nathan CASSEREAU <nathan.cassereau@idris.fr>
2023-09-22 09:31:41 +0000

    Tempfile

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -123,8 +123,7 @@ def main(args):
 
     model = train_loop(model, tokenizer, train_dataloader, criterion, optimizer)
     accuracy = eval_loop(model, tokenizer, test_dataloader)
-    print("max memory : ",torch.cuda.max_memory_allocated())
-
+    print("max memory : ", torch.cuda.max_memory_allocated())
 
 
 if __name__ == "__main__":
